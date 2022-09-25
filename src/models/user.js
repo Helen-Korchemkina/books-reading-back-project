f42e952970f0c78ae5cf2579ef464c601ddf67ad
Marian Kuzyk <mariankuzyk@gmail.com>
2022-09-26 00:59:35 +0300

    add google api

--- src/models/user.js
+++ src/models/user.js
@@ -5,39 +5,46 @@ const { handleSchemaValidationError } = require('../helpers');
 const emailRegexp = /^[\w.]+@[\w.]+.[\w.]+$/;
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is required'],
-    },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        match: emailRegexp,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true, 'Set password for user'],
-        minlength: 6,
-    },
-    token: {
-        type: String,
-        default: '',
-    },
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    match: emailRegexp,
+    unique: true,
+  },
+  password: {
+    type: String,
+    default: '',
+  },
+  token: {
+    type: String,
+    default: '',
+  },
+  startTraining: {
+    type: String,
+    default: '',
+  },
+  finishTraining: {
+    type: String,
+    default: '',
+  }
 }, { versionKey: false, timestamps: true });
 
 userSchema.post('save', handleSchemaValidationError);
 
 const joiRegisterSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().pattern(emailRegexp).required(),
-    password: Joi.string().min(6).required(),
-    confirm_password: Joi.string().required().valid(Joi.ref('password')),
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  confirm_password: Joi.string().required().valid(Joi.ref('password')),
 });
 
 const joiLoginSchema = Joi.object({
-    email: Joi.string().pattern(emailRegexp).required(),
-    password: Joi.string().min(6).required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
 });
 
 const schemas = {
@@ -48,6 +55,6 @@ const schemas = {
 const User = model('user', userSchema);
 
 module.exports = {
-    User,
-    schemas,
+  User,
+  schemas,
 };
