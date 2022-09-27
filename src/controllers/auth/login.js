5d4ceeadee13f730fe91c09756919108b725c8d8
Marian Kuzyk <mariankuzyk@gmail.com>
2022-09-27 15:52:11 +0300

    add validationBody and ctrlWrapper

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,46 +1,29 @@
 const { RequestError } = require("../../helpers");
-const { User, schemas } = require("../../models/user");
+const { User } = require("../../models/user");
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const { SECRET_KEY } = process.env;
 
-const login = async (req, res, next) => {
-    try {
-        const { error } = schemas.joiLoginSchema.validate(req.body);
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) { throw RequestError(401, 'Email or password is wrong') };
 
-        if (error) {
-            throw RequestError(400, error.message);
-        };
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) { throw RequestError(401, 'Email or password is wrong') };
 
-        const { email, password } = req.body;
-        const user = await User.findOne({ email });
+    const payload = {
+        id: user._id,
+    }
 
-        if (!user) {
-            throw RequestError(401, 'Email or password is wrong');
-        };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+    const data = await User.findByIdAndUpdate(user._id, { token }, { new: true, projection: { name: 1, email: 1 } });
 
-        const comparePassword = await bcrypt.compare(password, user.password);
-
-        if (!comparePassword) {
-            throw RequestError(401, 'Email or password is wrong');
-        };
-
-        const payload = {
-            id: user._id,
-        }
-
-        const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
-        const data = await User.findByIdAndUpdate(user._id, { token }, { new: true, projection: { name: 1 , email: 1 } });
-
-        res.json({
-            token,
-            data,
-        });
-
-    } catch (error) {
-        next(error);
-    };
+    res.json({
+        token,
+        data,
+    });
 };
 
 module.exports = login;
