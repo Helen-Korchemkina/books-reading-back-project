d7c92a4f92846c16029a62b2bec32fe6262f3d9f
Marian Kuzyk <mariankuzyk@gmail.com>
2022-10-03 21:20:38 +0300

    time of life token 100 days

--- src/service/user/createToken.js
+++ src/service/user/createToken.js
@@ -5,7 +5,7 @@ const { SECRET_KEY } = process.env;
 const createToken = async (id) => {
     const payload = { id };
 
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '100d' });
     const data = await User.findByIdAndUpdate(
         id,
         { token },
