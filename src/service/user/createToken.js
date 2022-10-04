1d677e5e3e4f37c67b11998689c33a1b28d0061a
Irina Skopetskaya <sairina2704@gmail.com>
2022-10-04 03:23:09 +0300

    updateSwagger

--- src/service/user/createToken.js
+++ src/service/user/createToken.js
@@ -2,18 +2,18 @@ const jwt = require('jsonwebtoken');
 const { User } = require('../../models');
 const { SECRET_KEY } = process.env;
 
-const createToken = async (id) => {
-    const payload = { id };
+const createToken = async id => {
+  const payload = { id };
 
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
-    const data = await User.findByIdAndUpdate(
-        id,
-        { token },
-        { new: true, projection: { name: 1, email: 1 } }
-    );
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '100d' });
+  const data = await User.findByIdAndUpdate(
+    id,
+    { token },
+    { new: true, projection: { name: 1, email: 1 } }
+  );
 
-    const allData = { token, data }
-    return allData;
+  const allData = { token, data };
+  return allData;
 };
 
 module.exports = createToken;
