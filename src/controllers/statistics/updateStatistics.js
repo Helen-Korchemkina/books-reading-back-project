e99529ada7e881e7bca13e96eeb3739bd0879ae0
AButsai <butsai@ukr.net>
2022-10-06 17:07:32 +0300

    update Statistics

--- src/controllers/statistics/updateStatistics.js
+++ src/controllers/statistics/updateStatistics.js
@@ -3,22 +3,20 @@ const { RequestError } = require('../../helpers');
 
 const updateStatistics = async (req, res) => {
   const { _id } = req.user;
-  const { readDate, readTime, numberOfPagesRead } = req.body;
+  const { readDate, numberOfPagesRead } = req.body;
   let body = {};
 
   const getStatistics = await statisticsService.getStatistics(_id);
   if (!getStatistics) throw RequestError(404, 'Bad request');
 
-  if (readDate === null || readTime === null || numberOfPagesRead === null) {
+  if (readDate === null || numberOfPagesRead === null) {
     body = {
       readDate: [],
-      readTime: [],
       numberOfPagesRead: [],
     };
   } else {
     body = {
       readDate: [...getStatistics.readDate, readDate],
-      readTime: [...getStatistics.readTime, readTime],
       numberOfPagesRead: [
         ...getStatistics.numberOfPagesRead,
         numberOfPagesRead,
