c55dfe3eeb769bda0ccc54d0b1f3639797ce3b73
Marian <90156656+mrkuzyk@users.noreply.github.com>
2022-10-08 20:42:01 +0300

    fix day

--- src/helpers/getTrainingTime.js
+++ src/helpers/getTrainingTime.js
@@ -2,7 +2,7 @@ const getTrainingTime = (value) => {
     const date = new Date(value);
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
-    const day = date.getDate();
+    const day = date.getDate() + 1;
     return (`${year}-${month}-${day}`);
 };
 
